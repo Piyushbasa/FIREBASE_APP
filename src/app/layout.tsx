05a47898e8e9f4faf05ce55d8362712eb901bd9c
Piyush Kumar Basa (PIYURI) <piyushkumarbasa@gmail.com>
2025-10-26 06:33:54 +0000

    the lower background was in dark make the background colourful with crop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,11 +22,11 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <Image
-          src="https://images.unsplash.com/photo-1500382017468-9049fed747ef?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw3fHxj colourful grass fields&ixlib=rb-4.1.0&q=80&w=1080"
-          alt="Colorful grass fields"
+          src="https://images.unsplash.com/photo-1464979925219-5381a5a8a184?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxjcm9wJTIwZmllbGRzfGVufDB8fHx8MTY5OTM0NjA0N3ww&ixlib=rb-4.1.0&q=80&w=1080"
+          alt="Colorful crop fields"
           fill
           className="-z-10 object-cover brightness-50"
-          data-ai-hint="grass field"
+          data-ai-hint="crop fields"
         />
         {children}
         <Toaster />
