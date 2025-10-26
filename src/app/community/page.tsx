47d8f55944fcd8a8ba8d5779c08319359ecae4fd
Piyush Kumar Basa (PIYURI) <piyushkumarbasa@gmail.com>
2025-10-26 08:25:55 +0000

    community also connected with DD kisan prodcast chanel , all language DD

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -39,6 +39,17 @@ const resources = [
     href: 'https://twitter.com/agrigoi',
     cta: 'Follow @agrigoi',
   },
+  {
+    title: 'DD Kisan on YouTube',
+    description: 'Watch live broadcasts, news, and informative programs from the official Doordarshan Kisan channel in various languages.',
+    icon: () => (
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 fill-current text-red-600">
+            <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+        </svg>
+    ),
+    href: 'https://www.youtube.com/@DDKisan',
+    cta: 'Watch on YouTube',
+  },
 ];
 
 
@@ -57,7 +68,7 @@ export default function CommunityPage() {
                 <Card key={index} className="flex flex-col">
                   <CardHeader>
                     <div className="flex items-start gap-4">
-                      <div className="bg-primary/20 p-2 rounded-lg">
+                      <div className="bg-primary/20 p-2 rounded-lg flex items-center justify-center w-10 h-10">
                         <resource.icon className="w-6 h-6 text-primary" />
                       </div>
                       <div>
