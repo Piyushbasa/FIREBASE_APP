ca46fe8f64525325ecf0724d33e78a437edb829a
Piyush Kumar Basa (PIYURI) <piyushkumarbasa@gmail.com>
2025-10-26 06:34:33 +0000

    not showing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,13 +21,15 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <Image
-          src="https://images.unsplash.com/photo-1464979925219-5381a5a8a184?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxjcm9wJTIwZmllbGRzfGVufDB8fHx8MTY5OTM0NjA0N3ww&ixlib=rb-4.1.0&q=80&w=1080"
-          alt="Colorful crop fields"
-          fill
-          className="-z-10 object-cover brightness-50"
-          data-ai-hint="crop fields"
-        />
+        <div className="fixed inset-0 -z-10">
+          <Image
+            src="https://images.unsplash.com/photo-1464979925219-5381a5a8a184?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxjcm9wJTIwZmllbGRzfGVufDB8fHx8MTY5OTM0NjA0N3ww&ixlib=rb-4.1.0&q=80&w=1080"
+            alt="Colorful crop fields"
+            fill
+            className="object-cover brightness-50"
+            data-ai-hint="crop fields"
+          />
+        </div>
         {children}
         <Toaster />
       </body>
